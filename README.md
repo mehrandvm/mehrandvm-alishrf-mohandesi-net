f55c1f26bc7930fc952c4490789907fe04b5ce1f
mehrandvm <amir.daneshvarmoein@gmail.com>
2023-01-30 17:54:53 +0330

    Front-end

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+1. clone this repo to your local machine
+1. copy `.example.env` to `.env` file: `cp .example.env .env`
+1. create a new database and add the database credentials to your `.env` file
+1. run `composer install`
+1. run `npm install && npm run dev`
+1. run `php artisan key:generate`
+1. run `php artisan ecommerce:install`
+1. run `php artisan serve` and then visit `http://127.0.0.1:8000/`
+1. credentials to access admin panel (email: `admin@admin.com`, password: `password`)
+1. after you login as admin, you can access the admin page from `http://127.0.0.1:8000/admin`
+-   Home Page
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the site
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Email: mehrandvm@gmail.com</p>
+                    <p>Website: <a href="https://mehrandvm.com">Personal Website</a></p>
+                    <p>Github: <a href="https://github.com/mehrandvm">@mehrandvm</a></p>
+                    <p>Twitter: <a href="https://twitter.com/mehrandvm">@mehrandvm</a></p>
+                    <p>LinkedIn: <a href="https://linkedin.com/in/mehrandvm">Mehran Daneshvar</a></p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; Mehran Daneshvar / Ali Sharif - All rights reserved 2019</p>
+    </div>
+</footer>
